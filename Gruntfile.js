046bf0ab101eaeba97469402fa0696fc37ff061d
ChrisWren <cthewren@gmail.com>
2013-07-09 08:51:27 -0700

    Removed unecessary tasks from Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,4 @@
-/*global module:false*/
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
   grunt.initConfig({
     // Task configuration.
@@ -16,32 +15,17 @@ module.exports = function(grunt) {
         unused: true,
         boss: true,
         eqnull: true,
+        node: true,
         globals: {}
-      },
-      gruntfile: {
-        src: 'Gruntfile.js'
       },
       lib_test: {
         src: ['*.js']
       }
-    },
-    watch: {
-      gruntfile: {
-        files: '<%= jshint.gruntfile.src %>',
-        tasks: ['jshint:gruntfile']
-      },
-      lib_test: {
-        files: '<%= jshint.lib_test.src %>',
-        tasks: ['jshint:lib_test', 'nodeunit']
-      }
     }
   });
 
-  // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
-  // Default task.
   grunt.registerTask('default', ['jshint']);
 
 };
