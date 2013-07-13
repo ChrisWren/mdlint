c88b0e85ab675127c16824f43af21cf90a00523b
ChrisWren <cthewren@gmail.com>
2013-07-12 22:20:59 -0700

    Added test devDependencies

---
+++ test/tests.js
@@ -0,0 +1,48 @@
+require('should');
+var spawn = require('child_process').spawn;
+
+describe('mdlint', function () {
+
+  it('should lint local markdown files that match a file glob', function (done) {
+    var lintProcess = spawn('node', ['./bin/mdlint', 'glob', 'test/fixtures/*.md']);
+    var logData = '';
+
+    lintProcess.stdout.on('data', function (data) {
+      logData += data;
+    });
+
+    lintProcess.stdout.on('close', function () {
+      logData.should.include('Error');
+      done();
+    });
+  });
+
+  it('should lint a README from a GitHub repo', function (done) {
+    var lintProcess = spawn('node', ['./bin/mdlint', 'repo', 'ChrisWren/mdlint']);
+    var logData = '';
+
+    lintProcess.stdout.on('data', function (data) {
+      logData += data;
+    });
+
+    lintProcess.stdout.on('close', function () {
+      logData.should.include('Markdown passed linting.');
+      done();
+    });
+  });
+
+  it('should lint READMEs from repos returned by a GitHub query', function (done) {
+    var lintProcess = spawn('node', ['./bin/mdlint', 'query', 'grunt']);
+    var logData = '';
+
+    lintProcess.stdout.on('data', function (data) {
+      logData += data;
+    });
+
+    lintProcess.stdout.on('close', function () {
+      logData.should.include('Markdown passed linting.');
+      done();
+    });
+  });
+
+});
