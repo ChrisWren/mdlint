f11c54dda3f53c3702a7fda21dd6f9aaf905a1c9
ChrisWren <cthewren@gmail.com>
2013-07-20 13:36:08 -0700

    Added support for 'javascript' code blocks, made methods testable by relocating outside module.exports

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-var fs = require('fs');
-var spawn = require('child_process').spawn;
+var fs      = require('fs');
+var spawn   = require('child_process').spawn;
 
+              require('colors');
 var program = require('commander');
-var request = require('request');
-var glob = require("glob");
-var _ = require('lodash');
 var esprima = require('esprima');
-require('colors');
+var glob    = require('glob');
+var _       = require('lodash');
+var request = require('request');
+
+
+var headers = {
+
+  // GitHub API requires the User-Agent Header to be set
+  'User-Agent': 'mdlint'
+};
+
+var tokenFile = __dirname + '/authtoken.txt';
 
 module.exports = function () {
-
-  var headers = {
-
-    // GitHub API requires the User-Agent Header to be set
-    'User-Agent': 'mdlint'
-  };
-
-  var tokenFile = __dirname + '/authtoken.txt';
 
   // Use Auth Token if present
   if (fs.existsSync(tokenFile)) {
@@ -54,7 +55,7 @@ module.exports = function () {
     .description('lints local markdown files that match a file glob')
     .action(function (fileGlob) {
       glob.sync(fileGlob).forEach(function (file) {
-        parseMarkdown(fs.readFileSync(file, 'utf8'), file);
+        lintMarkdown(fs.readFileSync(file, 'utf8'), file);
       });
     });
 
@@ -101,167 +102,193 @@ module.exports = function () {
   if (!program.args.length) {
     program.help();
   }
-
-  /**
-   * Fetches a README from GitHub
-   * @param  {String} repo URL of repo to fetch
-   */
-  function fetchREADME (repo) {
-    request({
-      uri: 'https://api.github.com/repos/' + repo + '/readme',
-      headers: _.extend(headers, {
-
-        // Get raw README content
-        'Accept': 'application/vnd.github.VERSION.raw'
-      })
-    },
-    function (error, response, body) {
-      if (!error && response.statusCode === 200) {
-        parseMarkdown(body, repo);
-      } else {
-        if (response.headers['x-ratelimit-remaining'] === '0') {
-          getAuthToken();
-        } else {
-          console.log('README for https://github.com/' + repo.blue + ' not found.'.red);
-          return;
-        }
-      }
-    });
-  }
-
-  /**
-   * Parses the JavaScript code blocks from the markdown file
-   * @param  {String} body Body of markdown file
-   * @param  {String} file Filename
-   */
-  function parseMarkdown (body, file) {
-    var splitCode = body.split('```');
-      splitCode.forEach(function (block, index) {
-        if (index % 2 === 0 ||
-
-            // Only parse js and json code blocks
-            splitCode[index].substr(0, 2).toLowerCase() !== 'js') {
-
-          // Delete the text in between code sections
-          delete splitCode[index];
-        }
-      });
-
-    if (file && !program.silent) {
-      console.log(file.blue.bold);
-    }
-    if (_.all(_.compact(splitCode), validateCodeBlock)) {
-      if (!program.silent) {
-        console.log('Markdown passed linting.\n'.green);
-      }
-    } else {
-      console.log('Markdown failed linting.\n'.red);
-    }
-  }
-
-  /**
-   * Validates that code blocks are valid JavaScript
-   * @param  {String} code A block of code from the markdown file
-   */
-  function validateCodeBlock (code) {
-    if (code.substr(0, 4).toLowerCase() === 'json') {
-      try {
-        JSON.stringify(code.slice(4));
-      } catch (e) {
-        console.log(e);
-        console.log(code);
-        return false;
-      }
-      return true;
-    } else {
-      code = preprocessCode(code.slice('js'.length).trim());
-      try {
-        esprima.parse(code, { tolerant: true });
-      } catch (e) {
-
-        // Get indeces from lineNumber and column
-        var line   = e.lineNumber - 1;
-        var column = e.column - 1;
-
-        // Highlight error in code
-        code = code.split('\n');
-
-        code[line] = code[line].slice(0, column).magenta +
-                     code[line][column].red +
-                     code[line].slice(column + 1).magenta;
-
-        code = code.join('\n');
-
-        console.log(e);
-        console.log(code);
-        return false;
-      }
-      return true;
-    }
-  }
-
-  /**
-   * Retrieves an auth token so that the user can exceed the uauthenticated rate limit
-   */
-  function getAuthToken () {
-    console.log('You have hit the rate limit for unauthenticated requests, please log in to raise your rate limit:\n'.red);
-
-    program.prompt('GitHub Username: ', function (user) {
-
-      console.log('\nAfter entering your password, hit return' + ' twice.\n'.green);
-      var authProcess = spawn('curl', [
-        '-u',
-        user,
-        '-d',
-        '{"scopes":["repo"],"note":"mdlint"}',
-        '-s',
-        'https://api.github.com/authorizations'
-      ], {
-        stdio: [process.stdin, 'pipe', process.stderr]
-      });
-
-      authProcess.stdout.setEncoding('utf8');
-      authProcess.stdout.on('data', function (data) {
-        var response = JSON.parse(data);
-        if (response.message) {
-          console.log(response.message.red + '\n');
-          process.exit();
-        } else {
-          fs.writeFileSync(tokenFile, response.token);
-          console.log('Authenticated :). Now try your lint again. \n'.green);
-          process.exit();
-        }
-      });
-    });
-  }
-
-  /**
-   * Preprocesses the code block and re-formats it to allow for psuedo code
-   * @param  {String} code A block of code from the markdown file
-   * @return {String} Processed code transformed from pseudo code
-   */
-  function preprocessCode (code) {
-
-    // Remove starting comments
-    if (code.indexOf('//') === 0) {
-      code = code.slice(code.indexOf('\n'));
-    }
-
-    // Starts with an object literal
-    if (code.indexOf('{') === 0) {
-      code = 'var json = ' + code + ';';
-
-    // Starts with an object property
-    }  else if (code.indexOf(': {') !== -1 && code.indexOf(': {') < code.indexOf('{')) {
-      code = 'var json = {' + code + '};';
-    }
-
-    // Starts with an anonymous function
-    if (code.indexOf('function') === 0) {
-      code = 'var func = ' + code + ';';
-    }
-
-    // Contains ...
-    return code.replace('...', '');
-  }
 };
+
+/**
+ * Fetches a README from GitHub
+ * @param  {String} repo URL of repo to fetch
+ */
+function fetchREADME (repo) {
+  request({
+    uri: 'https://api.github.com/repos/' + repo + '/readme',
+    headers: _.extend(headers, {
+
+      // Get raw README content
+      'Accept': 'application/vnd.github.VERSION.raw'
+    })
+  },
+  function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+      lintMarkdown(body, repo);
+    } else {
+      if (response.headers['x-ratelimit-remaining'] === '0') {
+        getAuthToken();
+      } else {
+        console.log('README for https://github.com/' + repo.blue + ' not found.'.red);
+        return;
+      }
+    }
+  });
+}
+
+/**
+ * [parseMarkdown description]
+ * @param  {String} markdownContent Content of markdown file
+ * @return {Array}                  Array of objects with a language and code property
+ */
+function parseMarkdown (markdownContent) {
+  var codeBlocks =  markdownContent
+
+  .split('```')
+
+  .filter(function (codeBlock, index) {
+
+    // Delete the text in between code sections
+    if (index % 2 === 0 ||
+
+        // Remove non JavaScript and JSON code blocks
+        (codeBlock.trim().toLowerCase().indexOf('js')         !== 0 &&
+         codeBlock.trim().toLowerCase().indexOf('javascript') !== 0)) {
+      return false;
+    }
+    return true;
+  })
+
+  .map(function (codeBlock) {
+    return {
+      lang: codeBlock.split('\n')[0].trim().toLowerCase(),
+      code: codeBlock.slice(codeBlock.indexOf('\n') + 1)
+    };
+  });
+
+  return codeBlocks;
+}
+
+/**
+ * Parses the JavaScript code blocks from the markdown file
+ * @param  {String} body Body of markdown file
+ * @param  {String} file Filename
+ */
+function lintMarkdown (body, file) {
+
+  if (!program.silent) {
+    console.log(file.blue.bold);
+  }
+  var codeBlocks = parseMarkdown(body);
+  if (_.all(_.compact(codeBlocks), validateCodeBlock)) {
+    if (!program.silent) {
+      console.log('Markdown passed linting.\n'.green);
+    }
+  } else {
+    console.log('Markdown failed linting.\n'.red);
+  }
+}
+
+/**
+ * Validates that code blocks are valid JavaScript
+ * @param  {Object} code A block of code from the markdown file containg the lang and code
+ */
+function validateCodeBlock (codeBlock) {
+  var lang = codeBlock.lang;
+  var code = codeBlock.code;
+
+  if (lang === 'json') {
+    try {
+      JSON.parse(code);
+    } catch (e) {
+      console.log(e);
+      console.log(code);
+      return false;
+    }
+    return true;
+  } else if (lang === 'js' || lang === 'javascript') {
+    code = preprocessCode(code);
+    try {
+      esprima.parse(code, { tolerant: true });
+    } catch (e) {
+
+      // Get indeces from lineNumber and column
+      var line   = e.lineNumber - 1;
+      var column = e.column - 1;
+
+      // Highlight error in code
+      code = code.split('\n');
+
+      code[line] = code[line].slice(0, column).magenta +
+                   code[line][column].red +
+                   code[line].slice(column + 1).magenta;
+
+      code = code.join('\n');
+
+      console.log(e);
+      console.log(code);
+      return false;
+    }
+    return true;
+  }
+}
+
+/**
+ * Retrieves an auth token so that the user can exceed the uauthenticated rate limit
+ */
+function getAuthToken () {
+  console.log('You have hit the rate limit for unauthenticated requests, please log in to raise your rate limit:\n'.red);
+
+  program.prompt('GitHub Username: ', function (user) {
+
+    console.log('\nAfter entering your password, hit return' + ' twice.\n'.green);
+    var authProcess = spawn('curl', [
+      '-u',
+      user,
+      '-d',
+      '{"scopes":["repo"],"note":"mdlint"}',
+      '-s',
+      'https://api.github.com/authorizations'
+    ], {
+      stdio: [process.stdin, 'pipe', process.stderr]
+    });
+
+    authProcess.stdout.setEncoding('utf8');
+    authProcess.stdout.on('data', function (data) {
+      var response = JSON.parse(data);
+      if (response.message) {
+        console.log(response.message.red + '\n');
+        process.exit();
+      } else {
+        fs.writeFileSync(tokenFile, response.token);
+        console.log('Authenticated :). Now try your lint again. \n'.green);
+        process.exit();
+      }
+    });
+  });
+}
+
+/**
+ * Preprocesses the code block and re-formats it to allow for partial code
+ * @param  {String} code A block of code from the markdown file
+ * @return {String} Processed code transformed from partial code
+ */
+function preprocessCode (code) {
+
+  // Remove starting comments
+  if (code.indexOf('//') === 0) {
+    code = code.slice(code.indexOf('\n'));
+  }
+
+  // Starts with an object literal
+  if (code.indexOf('{') === 0) {
+    code = 'var json = ' + code + ';';
+
+  // Starts with an object property
+  } else if (code.indexOf(': {') !== -1 && code.indexOf(': {') < code.indexOf('{')) {
+    code = 'var json = {' + code + '};';
+  }
+
+  // Starts with an anonymous function
+  if (code.indexOf('function') === 0) {
+    code = 'var func = ' + code + ';';
+  }
+
+  // Contains ...
+  return code.replace('...', '');
+}
